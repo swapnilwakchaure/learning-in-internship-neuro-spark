1213bef0ae5db923d3d729138bd1b6c10b8fa6c3
swapnilwakchaure <swapnilwakchaure2411999@gmail.com>
2023-11-22 18:32:47 +0530

    today's submit

--- Python/CodingBat/list1.py
+++ Python/CodingBat/list1.py
@@ -6,19 +6,29 @@ def first_last6(nums):
     else:
         print(False)
 
-first_last6([1, 2, 6]) # → True
-first_last6([6, 1, 2, 3]) # → True
-first_last6([13, 6, 1, 2, 3]) # → False
+# first_last6([1, 2, 6]) # → True
+# first_last6([6, 1, 2, 3]) # → True
+# first_last6([13, 6, 1, 2, 3]) # → False
 
 
 # 2. Given an array of ints, return True if the array is length 1 or more, and the first element and the last element are equal.
 
 def same_first_last(nums):
-    if nums[0] == nums[len(nums)-1]:
-        print(True)
-    else:
-        print(False)
+    print(len(nums) >= 1 and nums[0] == nums[-1])
 
-same_first_last([1, 2, 3]) # → False
-same_first_last([1, 2, 3, 1]) # → True
-same_first_last([1, 2, 1]) # → True
+# same_first_last([1, 2, 3]) # → False
+# same_first_last([1, 2, 3, 1]) # → True
+# same_first_last([1, 2, 1]) # → True
+
+
+# Given 2 arrays of ints, a and b, return True if they have the same first element or they have the same last element. Both arrays will be length 1 or more.
+
+def common_end(list1, list2):
+    print(list1 == list2)
+
+common_end([1, 2, 3], [7, 3]) # → True
+common_end([1, 2, 3], [7, 3, 2]) # → False
+common_end([1, 2, 3], [1, 3]) # → True
+common_end([1, 2, 3], [1, 2, 3]) # → True
+
+
